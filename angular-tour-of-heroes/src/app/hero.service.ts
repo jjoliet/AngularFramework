252495f1f1e8185a127831530ea53af34307b0aa
Jason Joliet <jjoliet@asu.edu>
2018-05-17 19:17:26 -0700

    Hero Service complete, message component and Service done

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
+/* Using prividedIn: 'root' injects creates a single instance that is shared
+  injecting into any class that asks for it */
 @Injectable({
   providedIn: 'root'
 })
-export class HeroService {
 
-  constructor() { }
+export class HeroService {
+  
+  getHeroes(): Observable<Hero[]> {
+  	//TODO: send the message after fetching the heroes
+  	this.messageService.add('HeroService: fetched heroes');
+  	return of(HEROES);
+  }
+
+  constructor(private messageService: MessageService) { }
 }
